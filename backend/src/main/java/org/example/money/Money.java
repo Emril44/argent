d4b551ec0ae0b46eeb02862d64a7c9a97bdbf09b
M. K. <42112702+Emril44@users.noreply.github.com>
2026-04-25 21:14:26 +0300

    feat: fully implement and test Money class

--- backend/src/main/java/org/example/money/Money.java
+++ backend/src/main/java/org/example/money/Money.java
@@ -1,18 +1,53 @@
 package org.example.money;
 
+import org.example.exceptions.InsufficientFundsException;
 import org.example.exceptions.NegativeMoneyException;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Objects;
 
 public class Money {
     private final BigDecimal moneyAmount;
-    public Money(BigDecimal moneyAmount) throws RuntimeException {
+    public Money(BigDecimal moneyAmount) throws NegativeMoneyException {
         if(moneyAmount.compareTo(BigDecimal.ZERO) < 0)
             throw new NegativeMoneyException("Money cannot be negative.");
 
-        this.moneyAmount = moneyAmount;
+        this.moneyAmount = moneyAmount.setScale(2, RoundingMode.HALF_UP);
     }
 
     public BigDecimal getMoneyAmount() {
         return moneyAmount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true; // if object is itself, it's true
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return moneyAmount.compareTo(money.moneyAmount) == 0; // should return 1, ergo true
+    }
+
+
+    @Override
+    public int hashCode() {
+        BigDecimal strippedMoney = moneyAmount.stripTrailingZeros();
+        return Objects.hashCode(strippedMoney);
+    }
+
+    public Money add(Money addedMoney) {
+        BigDecimal currentMoney = this.getMoneyAmount();
+        BigDecimal newMoney = addedMoney.getMoneyAmount();
+
+        return new Money(currentMoney.add(newMoney));
+    }
+
+    public Money subtract(Money subtractedMoney) {
+        BigDecimal currentMoney = this.getMoneyAmount();
+        BigDecimal subtractAmount = subtractedMoney.getMoneyAmount();
+
+        if(subtractAmount.compareTo(currentMoney) > 0) {
+            throw new InsufficientFundsException("Attempted to subtract " + subtractAmount + "from " + currentMoney + ". Negative balance is not allowed.");
+        }
+        return new Money(currentMoney.subtract(subtractAmount));
     }
 }
---
+++ backend/src/test/java/MoneyValueTest.java
@@ -0,0 +1,57 @@
+import org.example.exceptions.InsufficientFundsException;
+import org.example.exceptions.NegativeMoneyException;
+import org.example.money.Money;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+public class MoneyValueTest {
+    @Test
+    public void throwWhenMoneyNegative() {
+        Exception exception = assertThrows(NegativeMoneyException.class,() -> {
+            Money meMoney = new Money(new BigDecimal("-1"));
+        });
+    }
+
+    @Test
+    public void allowZeroMoney() {
+        assertDoesNotThrow(() -> {
+            Money meMoney = new Money(new BigDecimal(BigInteger.ZERO));
+        });
+    }
+
+    @Test
+    public void allowPositiveMoney() {
+        assertDoesNotThrow(() -> {
+            Money meMoney = new Money(new BigDecimal("123456"));
+        });
+    }
+
+    @Test
+    public void scaleFixed() {
+        Money meMoney = new Money(new BigDecimal("12.345"));
+        Money expectedMeMoney = new Money(new BigDecimal("12.35"));
+
+        assertEquals(meMoney, expectedMeMoney);
+    }
+
+    @Test
+    public void addMoney() {
+        Money meMoney = new Money(new BigDecimal("10"));
+        Money newMeMoney = meMoney.add(new Money(new BigDecimal("90")));
+
+        assertEquals(new Money(new BigDecimal("100")), newMeMoney);
+    }
+
+    @Test
+    public void throwWhenSubtractingTooMuch() {
+        Exception exception = assertThrows(InsufficientFundsException.class, () -> {
+            Money meMoney = new Money(new BigDecimal("100"));
+            Money badMeMoney = meMoney.subtract(new Money(new BigDecimal("200")));
+        });
+    }
+}
