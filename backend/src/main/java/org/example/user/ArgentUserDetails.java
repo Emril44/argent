3c71e497ec2ef34af3adf8d9e5f781d118ae20f2
M. K. <42112702+Emril44@users.noreply.github.com>
2026-09-18 12:09:08 +0300

    feat(security): implement ArgentUserDetails and ArgentUserDetailsService

---
+++ backend/src/main/java/org/example/user/ArgentUserDetails.java
@@ -0,0 +1,50 @@
+package org.example.user;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class ArgentUserDetails implements UserDetails {
+    private UserEntity userEntity;
+
+    public ArgentUserDetails(UserEntity userEntity) {
+        this.userEntity = userEntity;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return userEntity.getPasswordHash();
+    }
+
+    @Override
+    public String getUsername() {
+        return userEntity.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
