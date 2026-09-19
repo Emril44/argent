8d9e04aa29da905d67975e4e9e1e7c68784e4879
Emril44 <xnifl21@gmail.com>
2026-09-19 13:48:49 +0300

    feat(security): added  to SecurityConfig

--- backend/src/main/java/org/example/SecurityConfig.java
+++ backend/src/main/java/org/example/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,7 +14,6 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        System.out.println("filter chain worksios :o");
         httpSecurity
                 .authorizeHttpRequests((auths) -> auths
                         .requestMatchers("/auth/login").permitAll()
@@ -31,5 +31,10 @@ public class SecurityConfig {
                         )
                 );
         return httpSecurity.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
     }
 }
